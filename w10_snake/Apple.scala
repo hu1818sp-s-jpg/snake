616fc848dbc7935a81d30a4f17c39a99a7dbeb18
GabriellaKaagstroem <ga7108ge-s@student.lu.se>
2025-11-20 15:00:53 +0100

    Ny Apple klass

---
+++ w10_snake/Apple.scala
@@ -0,0 +1,20 @@
+package snake
+
+import java.awt.color
+
+class Apple(using ctx: SnakeGame, settings: Settings) //game och set är som variabler som refererar till objekten. Med using så hittar scala själv vilka obejkt som används. 
+
+  def teleportAfterSteps: Int =
+    settings.apple.teleportAfterSteps
+
+  def teleport(): Pos =
+    game.randomFreePos()
+
+  def draw(): Unit =
+    game.drawBlock(pos, settings.apple.color)
+
+  def erase(): Unit =
+    game.eraseBlock(pos)
+
+  def isOccupyingBlockAt(p: Pos): Boolean =
+    p == pos
