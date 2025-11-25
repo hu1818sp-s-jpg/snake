e5ee0e6b922ee7bf8a5c7edec2d1d8f844af02a3
Julia Hilding <julia.l.hilding@gmail.com>
2025-11-25 18:17:30 +0100

    ändringar i handlekey och points counter i player.scala

--- w10_snake/Player.scala
+++ w10_snake/Player.scala
@@ -8,26 +8,21 @@ class Player(
 ):
 
   def eatsApple(): Unit =
-    snake._nbrOfApples += 1
-    points += 10
+    if Apple.erase() then snake._nbrOfApples += 1 && points += 10
 
-  def takesStep(): Unit =
-    snake._nbrOfSteps += 1
-    points += 1
       
 
   def handleKey(key: String): Unit = 
     // om key ingår i keyMap så uppdatera snake.dir
     def update = 
-      if keyMap.dir.isDefinedAt(key) then { key match
+      if keyMap.dir.get(key) then { key match
 
-        case keyMap.dir.left => if snake.dir != East then snake.dir = West //kolla i Player.KeyMap dir (nyckel värde tabell) och uppdatera värdet i snake.dir om current direction != opposite
+        case Some(newDir) => if snake.dir != key && snake.dir.opposite != key then
+          snake.dir = snake.copy(dir = newDir)
+          
+           //kolla i Player.KeyMap dir (nyckel värde tabell) och uppdatera värdet i snake.dir om current direction != opposite
 
-        case keyMap.dir.right =>  if snake.dir != West then snake.dir = East
 
-        case keyMap.dir.up => if snake.dir != South then snake.dir = North
-        
-        case keyMap.dir.down => if snake.dir != North then snake.dir = South
 
         case _ => ()
       }
